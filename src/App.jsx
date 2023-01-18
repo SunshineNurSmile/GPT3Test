0da39ebee90b35da236bc083a3a198c32ba860f2
SunshineNURSmile <yuanyiw@bu.edu>
2023-01-18 15:59:51 -0500

    Changed env variable

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const chatbox = useRef(null);
   const input = useRef(null);
   const configuration = new Configuration({
-    apiKey: import.meta.env.OPENAI_API_KEY,
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
   const [prompt, setPrompt] = useState("");
